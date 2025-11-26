3b89efab9d8d81f490952bd73b50a6b43a565093
Himanshu Gondaliya <himanshupatel6895@gmail.com>
2025-11-26 17:22:41 +0530

    Update API baseURL to include '/api' path

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://spring-ecommerce-backend-production.up.railway.app",
+  baseURL: "https://spring-ecommerce-backend-production.up.railway.app/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
