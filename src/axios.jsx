344c275867dbd4c883841a01e7c473a6271839f7
Himanshu Gondaliya <himanshupatel6895@gmail.com>
2025-11-26 17:25:47 +0530

    Comment out Authorization header deletion
    Comment out the deletion of the Authorization header.

--- src/axios.jsx
+++ src/axios.jsx
@@ -3,5 +3,5 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "https://spring-ecommerce-backend-production.up.railway.app/api",
 });
-delete API.defaults.headers.common["Authorization"];
+// delete API.defaults.headers.common["Authorization"];
 export default API;
